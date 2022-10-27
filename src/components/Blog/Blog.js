2e741ef2b3c25dcfdb94d2193fd83e907e04c8c0
ahadulislampinoy <ahadulislampinoy@gmail.com>
2022-10-27 19:09:01 +0600

    blog done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,68 +7,104 @@ import reactRouterImgImg from "../../assets/react-router.png";
 const Blog = () => {
   return (
     <div>
-      <section class="grid grid-cols-1 md:grid-cols-2 gap-6 p-10 bg-gray-100">
-        <article class="bg-white rounded-2xl shadow-sm relative">
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-6 p-10 bg-gray-100">
+        <article className="bg-white rounded-2xl shadow-sm relative">
           <img
-            class="rounded-t-2xl h-60 w-full object-contain"
+            className="rounded-t-2xl h-60 w-full object-contain"
             src={expressImg}
             alt="img"
           />
-          <span class="absolute top-5 left-5 bg-gray-200 text-gray-900 px-3 py-1 rounded-full text-sm">
+          <span className="absolute top-5 left-5 bg-gray-200 text-gray-900 px-3 py-1 rounded-full text-sm">
             Express
           </span>
-          <div class="p-5">
-            <h5 class="font-semibold text-xl">What is cors?</h5>
-            <p class="text-gray-600">...</p>
+          <div className="p-5">
+            <h5 className="font-semibold text-xl">What is cors?</h5>
+            <p className="text-gray-600 mt-3 leading-7">
+              Cors stands for “Cross-Origin Resource Sharing”. It's a node.js
+              package. The web API's is a procedure for two programs to
+              communicate. That means API resources are consumed by clients and
+              servers. When the client and server have a different origin from
+              each other means when the client tries/requests to access
+              resources from a different server, the server will fail. It
+              happens for browser security, this is the time when we need to use
+              Cors, it will disable these mechanisms and allow the client to
+              access resources from the server.
+            </p>
           </div>
         </article>
-        <article class="bg-white rounded-2xl shadow-sm relative">
+        <article className="bg-white rounded-2xl shadow-sm relative">
           <img
-            class="rounded-t-2xl h-60 w-full object-contain"
+            className="rounded-t-2xl h-60 w-full object-contain"
             src={firebaseImg}
             alt="img"
           />
-          <span class="absolute top-5 left-5 bg-yellow-200 text-gray-900 px-3 py-1 rounded-full text-sm">
+          <span className="absolute top-5 left-5 bg-yellow-200 text-gray-900 px-3 py-1 rounded-full text-sm">
             Firebase
           </span>
-          <div class="p-5">
-            <h5 class="font-semibold text-lg">
+          <div className="p-5">
+            <h5 className="font-semibold text-lg">
               Why are you using firebase? What other options do you have to
               implement authentication?
             </h5>
-            <p class="text-gray-600">...</p>
+            <p className="text-gray-600 mt-3 leading-7">
+              Firebase is Google’s Backend-as-a-Service (BAAS). It offers many
+              features as a backend development tool for mobile and web apps. It
+              helps us to write less code for the backend, manage the database,
+              and so on. There are many other options for implementing
+              authentication, they are: <code>1.Okta</code>{" "}
+              <code>2.Amazon Cognito</code> <code>3.JSON Web Token</code>{" "}
+              <code>4.Keycloak</code> <code>5.Auth0</code>
+            </p>
           </div>
         </article>
-        <article class="bg-white rounded-2xl shadow-sm relative">
+        <article className="bg-white rounded-2xl shadow-sm relative">
           <img
-            class="rounded-t-2xl h-60 w-full object-contain"
+            className="rounded-t-2xl h-60 w-full object-contain"
             src={reactRouterImgImg}
             alt="img"
           />
-          <span class="absolute top-5 left-5 bg-red-200 text-gray-900 px-3 py-1 rounded-full text-sm">
+          <span className="absolute top-5 left-5 bg-red-200 text-gray-900 px-3 py-1 rounded-full text-sm">
             Route
           </span>
-          <div class="p-5">
-            <h5 class="font-semibold text-xl">
+          <div className="p-5">
+            <h5 className="font-semibold text-xl">
               How does the private route work?
             </h5>
-            <p class="text-gray-600">...</p>
+            <p className="text-gray-600 mt-3 leading-7">
+              Private Routes vary on the Apps, For example, Dashboard, User
+              Profile, App Settings, etc. In simple words, Routes that can be
+              accessed only after login are called private routes. So if a user
+              is not authorized for a specific page, they cannot access it. The
+              main difference between public and private route are redirect URL
+              and authenticate condition. If the user is not authorized will be
+              redirected to the login page.
+            </p>
           </div>
         </article>
-        <article class="bg-white rounded-2xl shadow-sm relative">
+        <article className="bg-white rounded-2xl shadow-sm relative">
           <img
-            class="rounded-t-2xl h-60 w-full object-contain"
+            className="rounded-t-2xl h-60 w-full object-contain"
             src={nodeImg}
             alt="img"
           />
-          <span class="absolute top-5 left-5 bg-green-200 text-gray-900 px-3 py-1 rounded-full text-sm">
+          <span className="absolute top-5 left-5 bg-green-200 text-gray-900 px-3 py-1 rounded-full text-sm">
             Node
           </span>
-          <div class="p-5">
-            <h5 class="font-semibold text-xl">
+          <div className="p-5">
+            <h5 className="font-semibold text-xl">
               What is Node? How does Node work?
             </h5>
-            <p class="text-gray-600">...</p>
+            <p className="text-gray-600 mt-3 leading-7">
+              Node.js is an open-source backend javascript runtime environment.
+              It launched in 2009 and is written in C, C++, and JavaScript. It
+              is used as a backend service where javascript works on the server
+              side of the application. This way javascript is used on both
+              frontend and backend. Node.js runs on chrome v8 engine which
+              converts javascript code into machine code. Node.js accepts the
+              request from the clients and sends the response, while working
+              with the request node.js handles them with a single thread to
+              operate input/output operations.
+            </p>
           </div>
         </article>
       </section>
--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -3,19 +3,19 @@ import React from "react";
 const Faq = () => {
   return (
     <div>
-      <div class="bg-[#f3f4f9] py-20 px-4 text-gray-900">
-        <div class="mx-auto max-w-6xl flex flex-col md:flex-row">
-          <h2 class="mr-8 mb-16 w-full md:w-1/3 text-4xl font-extrabold ">
+      <div className="bg-[#f3f4f9] py-20 px-4 text-gray-900">
+        <div className="mx-auto max-w-6xl flex flex-col md:flex-row">
+          <h2 className="mr-8 mb-16 w-full md:w-1/3 text-4xl font-extrabold ">
             Frequently-asked questions
           </h2>
-          <dl class="w-full md:w-2/3">
-            <dt class="mb-4">
-              <h3 class="text-xl font-semibold">
+          <dl className="w-full md:w-2/3">
+            <dt className="mb-4">
+              <h3 className="text-xl font-semibold">
                 Is Brighters an accredited institution? Do I receive anything
                 after I complete a course?
               </h3>
             </dt>
-            <dd class="mb-14">
+            <dd className="mb-14">
               <p>
                 While Brighters is not an accredited institution, we offer
                 skills-based courses taught by real-world experts in their
@@ -23,21 +23,23 @@ const Faq = () => {
                 completion to document your accomplishment.
               </p>
             </dd>
-            <dt class="mb-4">
-              <h3 class="text-xl font-semibold">How can I pay for a course?</h3>
+            <dt className="mb-4">
+              <h3 className="text-xl font-semibold">
+                How can I pay for a course?
+              </h3>
             </dt>
-            <dd class="mb-14">
+            <dd className="mb-14">
               <p>
                 Brighters supports several different payment methods, depending
                 on your account country and location.
               </p>
             </dd>
-            <dt class="mb-4">
-              <h3 class="text-xl font-semibold">
+            <dt className="mb-4">
+              <h3 className="text-xl font-semibold">
                 What if I don’t like a course I purchased?
               </h3>
             </dt>
-            <dd class="mb-14">
+            <dd className="mb-14">
               <p>
                 We want you to be satisfied, so all eligible courses purchased
                 on Brighters can be refunded within 30 days. If you are unhappy
@@ -45,13 +47,13 @@ const Faq = () => {
                 meets the guidelines in our refund policy.
               </p>
             </dd>
-            <dt class="mb-4">
-              <h3 class="text-xl font-semibold">
+            <dt className="mb-4">
+              <h3 className="text-xl font-semibold">
                 Will I continue to have access to the course even after I
                 complete it?
               </h3>
             </dt>
-            <dd class="mb-14">
+            <dd className="mb-14">
               <p>
                 Yes. You will continue to have access to the course after you
                 complete it, provided that your account’s in good standing, and
