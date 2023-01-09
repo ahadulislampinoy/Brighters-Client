79699d6b4b3de369441fe4ca40eba9de954f8410
ahadulislampinoy <ahadulislampinoy@gmail.com>
2023-01-10 04:16:42 +0600

    footer year set to dynamic

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="flex flex-col sm:flex-row justify-between items-center p-5 sm:p-10">
       <p className="text-center text-gray-800 font-medium pb-4 sm:pb-0">
-        &copy; 2022 Brighters. All rights reservered.
+        &copy; {year} Brighters. All rights reservered.
       </p>
       <div className="flex justify-center space-x-5">
         <a
@@ -26,7 +27,7 @@ const Footer = () => {
             src="https://img.icons8.com/fluent/30/000000/linkedin-2.png"
             alt=""
           />
-        </a>       
+        </a>
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
           <img
             src="https://img.icons8.com/fluent/30/000000/twitter.png"
